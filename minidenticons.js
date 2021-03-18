3ab9cade750dbe32f1c2ffbe6f7d3a25fe92a708
Laurent Payot <laurent.payot@publiberty.com>
2021-03-18 22:12:59 +0100

    minidenticons.js: spacing + renaming

--- minidenticons.js
+++ minidenticons.js
@@ -1,8 +1,10 @@
 const COLORS = [ '#d4145a', '#8e78ff', '#ff7300', '#fbb03b', '#ed1e79', '#019244', '#ed1c23', '#2e3192', '#fc7d7b', '#fecc00', '#3aa17e', '#4f00bc', '#09c9be', '#662d8c', '#00a8c5', '#1353ae' ]
-const COLORED_SQUARE_DENSITY = 4 // density of 4 for the lowest probability of collision
+const SQUARE_DENSITY = 4 // density of 4 for the lowest probability of collision
+
 // 32 bit FNV-1a hash parameters
 const FNV_PRIME = 16777619
 const OFFSET_BASIS = 2166136261
+
 
 // FNV1a-like hash function http://www.isthe.com/chongo/tech/comp/fnv/index.html
 function pseudoFNV1a(str) {
@@ -19,7 +21,7 @@ export function identicon(str) {
     const color = COLORS[(hash / FNV_PRIME) % COLORS.length] || ''
     const rects = !str ? [] : [...Array(25).keys()]
         // 2 + ((3 * 5 - 1) - modulo) to concentrate squares at the center
-        .map(i => hash % (16 - i % 15) < COLORED_SQUARE_DENSITY ?
+        .map(i => hash % (16 - i % 15) < SQUARE_DENSITY ?
             `<rect x="${i > 14 ? 7 - ~~(i/5) : ~~(i/5)}" y="${i % 5}" width="1" height="1"/>` : '')
         .join('')
     return `<svg viewBox="-1.5 -1.5 8 8" xmlns="http://www.w3.org/2000/svg" fill="${color}">${rects}</svg>`
