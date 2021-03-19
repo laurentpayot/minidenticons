8ab560a678ba637fe9b009e8b7b3c0e148e89c9b
Laurent Payot <laurent.payot@publiberty.com>
2021-03-19 09:34:53 +0100

    colors size optimization

--- minidenticons.js
+++ minidenticons.js
@@ -1,4 +1,5 @@
-const COLORS = [ '#d4145a', '#8e78ff', '#ff7300', '#fbb03b', '#ed1e79', '#019244', '#ed1c23', '#2e3192', '#fc7d7b', '#fecc00', '#3aa17e', '#4f00bc', '#09c9be', '#662d8c', '#00a8c5', '#1353ae' ]
+const COLORS = 'd4145a 8e78ff ff7300 fbb03b ed1e79 019244 ed1c23 2e3192 fc7d7b fecc00 3aa17e 4f00bc 09c9be 662d8c 00a8c5 1353ae'
+                .split(' ')
 const SQUARE_DENSITY = 4 // density of 4 for the lowest probability of collision
 
 // 32 bit FNV-1a hash parameters
@@ -25,7 +26,7 @@ export function identicon(str) {
             `<rect x="${i > 14 ? 7 - ~~(i/5) : ~~(i/5)}" y="${i % 5}" width="1" height="1"/>` : '')
         .join('')
         : []
-    return `<svg viewBox="-1.5 -1.5 8 8" xmlns="http://www.w3.org/2000/svg" fill="${color}">${rects}</svg>`
+    return `<svg viewBox="-1.5 -1.5 8 8" xmlns="http://www.w3.org/2000/svg" fill="#${color}">${rects}</svg>`
 }
 
 export const identiconSvg = globalThis.customElements?.define('identicon-svg',
--- tests.js
+++ tests.js
@@ -11,7 +11,7 @@ console.time("\nTests duration")
 
 // identicon tests
 
-assert.equal(identicon(""), '<svg viewBox="-1.5 -1.5 8 8" xmlns="http://www.w3.org/2000/svg" fill=""></svg>')
+assert.equal(identicon(""), '<svg viewBox="-1.5 -1.5 8 8" xmlns="http://www.w3.org/2000/svg" fill="#"></svg>')
 assert.equal(identicon("foo"), '<svg viewBox="-1.5 -1.5 8 8" xmlns="http://www.w3.org/2000/svg" fill="#2e3192"><rect x="1" y="1" width="1" height="1"/><rect x="2" y="0" width="1" height="1"/><rect x="2" y="1" width="1" height="1"/><rect x="2" y="2" width="1" height="1"/><rect x="2" y="3" width="1" height="1"/><rect x="2" y="4" width="1" height="1"/><rect x="3" y="1" width="1" height="1"/></svg>')
 
 
