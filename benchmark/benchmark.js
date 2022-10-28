416a44e459c228709df1cf1ce4a9eff0f61dbaed
Laurent Payot <laurent.payot@publiberty.com>
2022-10-28 12:00:27 +0200

    benchmark: improved randomUsername()

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,18 +1,22 @@
 import { identicon } from '../minidenticons.min.js'
 
-export const USERNAME_LENGTH = 10
+export const USERNAME_LENGTH = 15
 export const IDENTICONS_PER_PAGE = 300
 export const RUNS_NUMBER = 1_000
 
-function randomUsername() {
-    return Math.random().toString(36).substring(2).padEnd(USERNAME_LENGTH, "0")
+function randomUsername(length) {
+    let str = ""
+    while (str.length < length) {
+        str += Math.random().toString(36).substring(2)
+    }
+    return str.substring(0, length)
 }
 
 export function benchmark() {
     const durations = []
     let username, t0, t1
     for (let i = 0; i < IDENTICONS_PER_PAGE * RUNS_NUMBER; i++) {
-        username = randomUsername()
+        username = randomUsername(USERNAME_LENGTH)
         t0 = performance.now()
         identicon(username)
         t1 = performance.now()
