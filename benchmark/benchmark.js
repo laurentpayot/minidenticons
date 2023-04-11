d966dd850c87ad1ff57a58ca4685f329659e87ca
Laurent Payot <laurent.payot@publiberty.com>
2023-04-11 18:38:04 +0200

    custom element browser benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,27 +1,24 @@
-import { identicon } from '../minidenticons.min.js'
+export function benchmark(testFn, testArgsFn , breakFn, testNb=100, runNb=10) {
+    const durations = []
+    let t0, t1, args
+    [...Array(runNb)].forEach((_, i) => {
+        breakFn?.(i + 1)
+        ;[...Array(testNb)].forEach(() => {
+            args = testArgsFn()
+            t0 = performance.now()
+            testFn(...args)
+            t1 = performance.now()
+            durations.push(t1 - t0)
+        })
+    })
+    const averageRunDuration = durations.reduce((acc, d) => acc + d, 0) / runNb
+    return averageRunDuration
+}
 
-export const USERNAME_LENGTH = 15
-export const IDENTICONS_PER_PAGE = 300
-export const RUNS_NUMBER = 100
-
-function randomUsername(length) {
+export function randomString(length) {
     let str = ""
     while (str.length < length) {
         str += Math.random().toString(36).substring(2)
     }
     return str.substring(0, length)
 }
-
-export function benchmark() {
-    const durations = []
-    let username, t0, t1
-    [...Array(IDENTICONS_PER_PAGE * RUNS_NUMBER)].forEach(() => {
-        username = randomUsername(USERNAME_LENGTH)
-        t0 = performance.now()
-        identicon(username)
-        t1 = performance.now()
-        durations.push(t1 - t0)
-    })
-    const averageRunDuration = durations.reduce((acc, d) => acc + d, 0) / RUNS_NUMBER
-    return averageRunDuration
-    }
--- benchmark/node.js
+++ benchmark/node.js
@@ -1,15 +1,25 @@
-import { USERNAME_LENGTH, IDENTICONS_PER_PAGE, RUNS_NUMBER, benchmark } from './benchmark.js'
+import { identicon } from '../minidenticons.min.js'
+import { benchmark, randomString } from './benchmark.js'
 
+const TEST_NB = 5_000
+const RUN_NB = 10
+const USERNAME_LENGTH = 15
 
-const averageRunDuration = benchmark()
+const averageRunDuration = benchmark(
+    (username) => identicon(username),
+    () => [ randomString(USERNAME_LENGTH) ],
+    (run) => console.log(`Run ${run} of ${RUN_NB}…`),
+    TEST_NB,
+    RUN_NB
+)
 
 console.log(
     `\nTime to generate ${
-        IDENTICONS_PER_PAGE
+        TEST_NB
     } identicon SVG strings for ${
         USERNAME_LENGTH
     } characters random usernames:\n${
         Math.round(averageRunDuration)
     } milliseconds (${
-        RUNS_NUMBER
+        RUN_NB
     } runs average)\n`)
